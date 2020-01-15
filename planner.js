7cffd2c8242a269f906b8f07b9acf165f4540f94
Marcia <marciaadler718@gmail.com>
2020-01-15 09:28:50 -0500

    updated code to remove console logs and unused code

--- planner.js
+++ planner.js
@@ -11,11 +11,11 @@ $(document).ready(function() {
     // populate activity-input text-area with array values by id.
     if(localStorage.getItem('activities')){
         for (let i = 0; i < parseActivities.length; i++) {
-            console.log(parseActivities[i].timeOfDay);
+            
             
             
             $('#'+hours[i]).val(parseActivities[i].title);
-            console.log(parseActivities[i].title);
+            
         }
     } 
     // loop through all the hours and assign an index to the save button
@@ -24,12 +24,7 @@ $(document).ready(function() {
          
         event.preventDefault();
         // save all inputs on page
-        // getting the value from the last button clicked data index value
-        //var index = $(this).attr('data-index');
-        // grabbing the value from the hours array using data-index from save button
-        //var text = hours[index];
-        // grabbing the text from textarea by id determine by var text
-        //var input = $(`#${text}`);
+        
        
         // go through each inpuit
         inputs = [];
@@ -55,25 +50,28 @@ $(document).ready(function() {
     // color code calendar
     // grab the current time
     var currentTime = parseInt(moment().hour());
-    console.log(currentTime);
+    
     // compare to labels on form
     $('.activity-input').each(function(){
+
+        // if time is the same make the textarea red
         if (currentTime === parseInt($(this).attr('data-index'))){
             $(this).addClass('current-time');
+            // if the time is in the future, make the textarea green
         } else if (currentTime < parseInt($(this).attr('data-index'))){
             $(this).addClass('future-time');
         } else {
+            // if the time has past, make the textarea grey
             $(this).addClass('past-time');
         }
-        console.log(`${$(this).attr('data-index')} ${currentTime}`)
         
-    // if time is the same make the textarea red
-    // if the time has past, make the textarea grey
-    // if the time is in the future, make the textarea green
+        
+    
+    
     }) 
 
     // change opacity of save button image
-    // on mouseover
+    
     $('img').hover(
         function() {
         $(this).stop().animate({"opacity": ".6"}, "slow");
